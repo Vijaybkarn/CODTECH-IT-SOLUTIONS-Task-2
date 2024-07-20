1e6643031d4af2cd38210977eb3d5bfffde91c4f
SHUBHAM KUMAR <142647226+Vijaybkarn@users.noreply.github.com>
2024-07-20 12:56:49 +0530

    Update script.js

--- Weather Forecast/script.js
+++ Weather Forecast/script.js
@@ -1,6 +1,6 @@
 
 function getWeather() {
-    const apiKey = '4ec3217129bb9cc3acce9b4f976c91c7';
+    const apiKey = '4ec3217129bb9cc3acce9b4f976c91c7';//Add your Key from openweather for API
     const city = document.getElementById('city').value;
 
     if (!city) {
@@ -9,9 +9,7 @@ function getWeather() {
     }
 
     
-    /*const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
-*/
+   
     const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=metric`;
     const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}&units=metric`;
     
@@ -53,7 +51,7 @@ function displayWeather(data) {
         weatherInfoDiv.innerHTML = `<p>${data.message}</p>`;
     } else {
         const cityName = data.name;
-        const temperature = Math.round(data.main.temp - 273.15); // Convert to Celsius
+        const temperature = Math.round(data.main.temp - 273.15); 
         const description = data.weather[0].description;
         const iconCode = data.weather[0].icon;
         const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
@@ -79,12 +77,12 @@ function displayWeather(data) {
 function displayHourlyForecast(hourlyData) {
     const hourlyForecastDiv = document.getElementById('hourly-forecast');
 
-    const next24Hours = hourlyData.slice(0, 8); // Display the next 24 hours (3-hour intervals)
+    const next24Hours = hourlyData.slice(0, 8);
 
     next24Hours.forEach(item => {
-        const dateTime = new Date(item.dt * 1000); // Convert timestamp to milliseconds
+        const dateTime = new Date(item.dt * 1000); 
         const hour = dateTime.getHours();
-        const temperature = Math.round(item.main.temp - 273.15); // Convert to Celsius
+        const temperature = Math.round(item.main.temp - 273.15); 
         const iconCode = item.weather[0].icon;
         const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
 
